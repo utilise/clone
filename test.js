00212d22e29f99ab974c9fbd1539d4f9c5f8a365
pemrouz <p3pedram@hotmail.com>
2015-06-14 15:42:53 +0100

    init commit

---
+++ test.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , clone = require('./')
+
+describe('clone', function() {
+  
+  it('should clone object', function() {
+    var a = {}
+      , b = clone(a)
+
+    b.foo = 'bar'
+
+    expect(a).to.be.eql({})
+    expect(b).to.be.eql({ foo: 'bar' })
+  })
+  
+  it('should ignore functions', function() {
+    var a = String
+      , b = clone(a)
+
+    b.foo = 'bar'
+
+    expect(a.foo).to.be.eql('bar')
+    expect(b.foo).to.be.eql('bar')
+  })
+
+})
